92ec5a59730fded07738d81657c654a84587e7ff
Miguel Oliveira <Miguel Oliveira>
2025-06-20 14:40:20 -0300

    testando filesys lib, criando um diretório e verificando o caminho atual

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+
 using namespace std;
+using namespace std::filesystem;
+
 int main(){
-    cout << "Hello";
+    path dir = "mydir";
+    if(!exists(dir)){
+        create_directories(dir);
+        cout << "Directory created: " << dir << endl;
+    }
+    cout << "Current path is: " << current_path() << endl;
 }
