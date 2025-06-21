1ef42e5d2bcd4e6dc5a4b656395ef782e9aefca1
Miguel Oliveira <Miguel Oliveira>
2025-06-21 15:04:32 -0300

    percorre e lista os itens do diretório

--- main.cpp
+++ main.cpp
@@ -3,13 +3,38 @@
 #include <fstream>
 
 using namespace std;
-using namespace std::filesystem;
+namespace fs = std::filesystem;
+using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
 
+void check_current_directory() {
+    try {
+        fs::path dir = fs::current_path();
+        if (fs::is_empty(dir)) {
+            std::cout << "Current directory is empty." << std::endl;
+        } else {
+            std::cout << "Current directory is not empty." << std::endl;
+        }
+    } catch (const fs::filesystem_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
 int main(){
-    path dir = "mydir";
+    fs::path dir = "mydir";
     if(!exists(dir)){
         create_directories(dir);
         cout << "Directory created: " << dir << endl;
     }
-    cout << "Current path is: " << current_path() << endl;
+    check_current_directory(); 
+    try {
+        for (const auto& dirEntry : fs::recursive_directory_iterator(fs::current_path())) {
+            if (dirEntry.path().string().find(".git") != std::string::npos)
+                continue; 
+
+            cout << dirEntry.path().filename() << endl;
+        }
+    } catch (const fs::filesystem_error& e) {
+        cerr << "Erro ao percorrer diretórios: " << e.what() << endl;
+    }
+
+    return 0;
 }
